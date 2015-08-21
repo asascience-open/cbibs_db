505dfba06c74a9bc5a506003f9a47140024790a7
Benjamin Adams <ben.adams@rpsgroup.com>
2015-08-21 15:33:20 -0400

    Add verify scripts

---
+++ verify/daily_counts.sql
@@ -0,0 +1,7 @@
+-- Verify cbibs_db:daily_counts on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
---
+++ verify/fix_standard_names.sql
@@ -0,0 +1,7 @@
+-- Verify cbibs_db:fix_standard_names on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
